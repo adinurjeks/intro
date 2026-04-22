47b801c49fc3360f65f5d78900c3e74e5d5a1f1e
adinurjeks <korolorol26@gmail.com>
2026-04-22 11:20:53 +0600

    создаем цикл на проверку каждого символа чтобы узнать являится ли он цифрой или буквой и плюсуем их к нашим заранее написанным переменным

--- adinur.py
+++ adinur.py
@@ -1,3 +1,9 @@
 def check(password):
     digits = 0
     letters = 0
+
+    for symbol in password:
+        if symbol.isdigit():
+            digits += 1
+        elif symbol.isalpha():
+            letters += 1
